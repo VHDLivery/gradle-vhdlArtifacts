3ef6ff337632fbb7d8f07400077a02f2ef9442d2
LogicLlama <steffen.romberg@icloud.com>
2025-01-05 16:20:31 +0100

    Improved publishing (local)

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     `java-gradle-plugin`
     alias(libs.plugins.kotlin.jvm)
+    `maven-publish`
 }
 
-val groupId = "com.github.logicllama.gradle.vhdl"
+group = "com.github.logicllama.gradle.vhdl"
+version = "1.0.0"
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -19,9 +21,16 @@ dependencies {
 
 gradlePlugin {
     // Define the plugin
-    val greeting by plugins.creating {
-        id = groupId
-        implementationClass = "${groupId}.VhdlPlugin"
+    website = "https://github.com/LogicLlama"
+    vcsUrl = "https://github.com/LogicLlama/gradle-vhdl"
+    plugins {
+        create("VhdlPlugin") {
+            id = "${group}"
+            displayName = "Plugin for FPGA Development with VHDL"
+            description = "A plugin that helps you manage dependencies"
+            tags = listOf("hdl", "vhdl")
+            implementationClass = "${group}.VhdlPlugin"
+        }
     }
 }
 
@@ -49,4 +58,11 @@ tasks.named<Task>("check") {
 tasks.named<Test>("test") {
     // Use JUnit Jupiter for unit tests.
     useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+    }
+    // To publish officially check https://docs.gradle.org/current/userguide/publishing_gradle_plugins.html
 }
